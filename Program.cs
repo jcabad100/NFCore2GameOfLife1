c7bf3ddacaa4739da6f11d677481ac53285fc590
Juan Carlos Abad <jcabad100@yahoo.com>
2022-01-29 01:58:57 +0800

    Trying to add Graphics
    Manually added reference to nanoFramework.ResourceManager but still "using nanoFramework.ResourceManager;" does not resolve and i still get "'Resource' is inaccessible due to its protection level" eg in this lline of code: "Font DisplayFont = Resource.GetFont(Resource.FontResources.SegoeUIRegular12);"

--- Program.cs
+++ Program.cs
@@ -7,7 +7,10 @@ using nanoFramework;
 using nanoFramework.M5Stack;
 using nanoFramework.M5Core2;
 using Iot.Device.Ft6xx6x;
-
+//using nanoFramework.Hardware.Esp32;
+using nanoFramework.UI;
+using nanoFramework.Presentation;
+using nanoFramework.ResourceManager;
 
 namespace NFCore2GameOfLife1
 {
@@ -15,6 +18,8 @@ namespace NFCore2GameOfLife1
     {
         public static void Main()
         {
+            int delayBetween = 1100;
+
             Debug.WriteLine("Hello from nanoFramework!");
             M5Core2.InitializeScreen();
 
@@ -38,9 +43,57 @@ namespace NFCore2GameOfLife1
             // This will enable an event on GPIO39 on falling edge when the screen if touched
             gpio.RegisterCallbackForPinValueChangedEvent(39, PinEventTypes.Falling, TouchInterrupCallback);
 
+
+            // Get full screen bitmap from displayControl to draw on.
+            nanoFramework.UI.Bitmap fullScreenBitmap = DisplayControl.FullScreen;
+
+            fullScreenBitmap.Clear();
+
+            Font DisplayFont = Resource.GetFont(Resource.FontResources.SegoeUIRegular12);
+
             while (true)
             {
-                Thread.Sleep(20);
+                RandomDrawLine rdlt = new RandomDrawLine(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                RotateImage ri = new RotateImage(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                ColourGradient colourGradient = new ColourGradient(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                Colours ColourExample = new Colours(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                PagedText pt = new PagedText(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                BouncingBalls bb = new BouncingBalls(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                TileImage ti = new TileImage(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                StretchImage si = new StretchImage(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                SetPixels sp = new SetPixels(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                FontExamples fe = new FontExamples(fullScreenBitmap);
+                Thread.Sleep(delayBetween);
+
+                RandomRectangles rr = new RandomRectangles(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                SliceScaling9 ss = new SliceScaling9(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                RandomClipping rc = new RandomClipping(fullScreenBitmap, DisplayFont);
+                Thread.Sleep(delayBetween);
+
+                MatrixRain mr = new MatrixRain(fullScreenBitmap);
+                Thread.Sleep(Timeout.Infinite);
             }
 
             void TouchInterrupCallback(object sender, PinValueChangedEventArgs pinValueChangedEventArgs)
